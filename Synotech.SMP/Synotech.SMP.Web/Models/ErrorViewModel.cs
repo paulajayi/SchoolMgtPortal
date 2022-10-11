13dad71b09c611b58b9fb522628559febbd74855
Paul Ajayi <paulo.ajayi@gmail.com>
2022-10-11 11:56:53 +0400

    onion architecture

---
+++ Synotech.SMP/Synotech.SMP.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Synotech.SMP.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

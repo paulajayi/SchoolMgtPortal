79d326d7ba7239c45e88c7e04ac1473e9faf3418
Paul Ajayi <paulo.ajayi@gmail.com>
2022-10-19 08:41:28 +0400

    TDD Project

---
+++ Synotech.SMP/Synotech.SMP.Web.UnitTests/Areas/Portal/Controllers/DashboardControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Synotech.SMP.Web;
+using Synotech.SMP.Web.Areas.Portal.Controllers;
+
+
+namespace Synotech.SMP.Web.UnitTests.Areas.Portal.Controllers
+{
+    
+    public class DashboardControllerTests
+    {
+
+        [Fact]
+        public void TestDashboard()
+        {
+            DashboardController dashboardController = new DashboardController();
+            ViewResult? result = dashboardController.Index() as ViewResult;
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void TestDashboardTitleMessage()
+        {
+            DashboardController dashboardController = new DashboardController();
+            ViewResult? result = dashboardController.Index() as ViewResult;
+            Assert.Equal("Dashboard", result.ViewData["Title"]);
+        }
+
+    }
+}
---
+++ Synotech.SMP/Synotech.SMP.Web.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+
+
+namespace Synotech.SMP.Web.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

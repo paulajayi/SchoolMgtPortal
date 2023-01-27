1adaf0ae503796e729e30276569ffffcaf1f6f4b
Paul Ajayi <paulo.ajayi@gmail.com>
2023-01-27 13:54:45 +0400

    UserService

---
+++ Synotech.SMP/Synotech.SMP.Core.Services.Interfaces/User/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synotech.SMP.Core.Services.Interfaces.User
+{
+    public interface IUserService
+    {
+        bool AuthenticateUser(string username, string password);
+    }
+}
---
+++ Synotech.SMP/Synotech.SMP.Core.Services/User/UserService.cs
@@ -0,0 +1,17 @@
+﻿using Synotech.SMP.Core.Services.Interfaces.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synotech.SMP.Core.Services.User
+{
+    public class UserService : IUserService
+    {
+        public bool AuthenticateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

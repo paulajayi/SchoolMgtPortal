3755b7a37df869b79a7a528577fb959cd28ac823
Paul Ajayi <paulo.ajayi@gmail.com>
2023-02-10 00:45:48 +0400

    AuthenticateUser

--- Synotech.SMP/Synotech.SMP.Core.Services/User/UserService.cs
+++ Synotech.SMP/Synotech.SMP.Core.Services/User/UserService.cs
@@ -1,20 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Synotech.SMP.Core.Interfaces.User;
 using Synotech.SMP.Core.Models.User;
-
+using Synotech.SMP.Infrastructure.Data;
+using Synotech.SMP.Util;
 namespace Synotech.SMP.Core.Services.User
 {
 
 
     public class UserService : IUserService
     {
-        public Task<UserLoginResponse> AuthenticateUser(string username, string password)
+        private readonly AppDbContext _dbContext;
+
+        public UserService(AppDbContext dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
+        }
+
+        public async Task<UserLoginResponse> AuthenticateUser(string username, string password)
+        {
+            var result = await _dbContext.Users.FirstOrDefaultAsync(x => x.Email == username && x.Password == Encryptor.SHA512(password));
+          
+            if (result != null)
+            {
+                var retVal = new UserLoginResponse
+                {
+                    Email = result.Email,
+                    Password = result.Password,
+                    IsActive = result.IsActive,
+                };
+
+                return retVal;
+            }
+
+            return null;
+
+            
+
+           
         }
     }
 }

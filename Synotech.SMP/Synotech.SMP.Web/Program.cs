01c597b1acf3c770b8554de05e2cebeda725601f
Paul Ajayi <paulo.ajayi@gmail.com>
2023-02-10 15:46:50 +0400

    Startup.cs

--- Synotech.SMP/Synotech.SMP.Web/Program.cs
+++ Synotech.SMP/Synotech.SMP.Web/Program.cs
@@ -1,68 +1,120 @@
-using SmartBreadcrumbs.Extensions;
-using System.Reflection;
-using Synotech.SMP.Core.Services.User;
-using Synotech.SMP.Core.Interfaces.User;
+//using SmartBreadcrumbs.Extensions;
+//using System.Reflection;
+//using Synotech.SMP.Core.Services.User;
+//using Synotech.SMP.Core.Interfaces.User;
 
-var builder = WebApplication.CreateBuilder(args);
+//var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddBreadcrumbs(Assembly.GetExecutingAssembly(), options =>
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddBreadcrumbs(Assembly.GetExecutingAssembly(), options =>
+//{
+//    options.TagName = "nav";
+//    options.TagClasses = "";
+//    options.OlClasses = "breadcrumb";
+//    options.LiClasses = "breadcrumb-item";
+//    options.ActiveLiClasses = "breadcrumb-item active";
+//});
+
+//builder.Services.AddScoped<IUserService, UserService>();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+
+
+
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+
+
+//app.UseAuthorization();
+
+////app.MapControllerRoute(
+////    name: "default",
+////    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapAreaControllerRoute(
+//       name: "Portal",
+//       areaName: "Portal",
+//       pattern: "Portal/{controller=Home}/{action=Index}/{id?}"
+
+//   );
+
+//    endpoints.MapControllerRoute(
+//      name: "areas",
+//      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+//    );
+
+//    endpoints.MapControllerRoute(
+//        name: "default",
+//        pattern: "{controller=Home}/{action=Index}/{id?}"
+//    );
+//});
+
+//app.Run();
+
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using Synotech.SMP.Infrastructure.Data;
+using Synotech.SMP.Web;
+
+namespace Synotech.SMP
 {
-    options.TagName = "nav";
-    options.TagClasses = "";
-    options.OlClasses = "breadcrumb";
-    options.LiClasses = "breadcrumb-item";
-    options.ActiveLiClasses = "breadcrumb-item active";
-});
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
 
-builder.Services.AddScoped<IUserService, UserService>();
+            CreateDbIfNotExists(host);
 
-var app = builder.Build();
+            host.Run();
+        }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+
+    }
 }
-
-
-
-
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-
-
-app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapAreaControllerRoute(
-       name: "Portal",
-       areaName: "Portal",
-       pattern: "Portal/{controller=Home}/{action=Index}/{id?}"
-
-   );
-
-    endpoints.MapControllerRoute(
-      name: "areas",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
-
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-    );
-});
-
-app.Run();
---
+++ Synotech.SMP/Synotech.SMP.Web/Startup.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using SmartBreadcrumbs.Extensions;
+using Synotech.SMP.Core.Interfaces.User;
+using Synotech.SMP.Core.Services.User;
+using Synotech.SMP.Infrastructure.Data;
+using System.Reflection;
+
+namespace Synotech.SMP.Web
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddScoped<IUserService, UserService>();
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddControllersWithViews();
+            services.AddBreadcrumbs(Assembly.GetExecutingAssembly(), options =>
+            {
+                options.TagName = "nav";
+                options.TagClasses = "";
+                options.OlClasses = "breadcrumb";
+                options.LiClasses = "breadcrumb-item";
+                options.ActiveLiClasses = "breadcrumb-item active";
+            });
+
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+              //  app.UseDeveloperExceptionPage();
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapAreaControllerRoute(
+                   name: "Portal",
+                   areaName: "Portal",
+                   pattern: "Portal/{controller=Home}/{action=Index}/{id?}"
+
+               );
+
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
+
+
+        }
+    }
+}
